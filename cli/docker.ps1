7f58b60245b5d0db6ecc3ca8ac1a376e895b1628
Marcin Narloch <marcin.narloch@qbituniverse.com>
2023-10-27 09:19:45 +0200

    sudo

--- cli/docker.ps1
+++ cli/docker.ps1
@@ -1,17 +1,24 @@
 [CmdletBinding()]
 Param
 (
-	[Parameter(Position=0,Mandatory=$false,HelpMessage="Operation: --view | --pd | --pu | --pai | --pav | --pan | --pac | --p-all")]
-	[string]$Operation
+	[Parameter(Position=0,Mandatory=$true,HelpMessage="Operation: --h | --v | --pd | --pu | --pai | --pav | --pan | --pac | --p-all")]
+	[string]$Operation,
+	[Parameter(Position=1,Mandatory=$false,HelpMessage="Switch: --s")]
+	[string]$Switch
 )
 
-If ($Operation -in "--help") {
+$AddSudo = $false;
+
+If ($Switch -in "--s") { $AddSudo = $true }
+
+If ($Operation -in "--h") {
 	Write-Host -ForegroundColor Green ""
 	Write-Host -ForegroundColor Green "Docker Environment:"
 	Write-Host -ForegroundColor Green ""
 	Write-Host -ForegroundColor Green "	[operation]"
 	Write-Host -ForegroundColor Green ""
-	Write-Host -ForegroundColor Green "	--view			View all docker resources: system stats, network, all images, all containers, all volumes, dangling volumes, dangling images."
+	Write-Host -ForegroundColor Green "	--h 			Help."
+	Write-Host -ForegroundColor Green "	--v 			View all docker resources: system stats, network, all images, all containers, all volumes, dangling volumes, dangling images."
 	Write-Host -ForegroundColor Green "	--pd			Prune dangling volumes & images."
 	Write-Host -ForegroundColor Green "	--pu			Prune unused volumes, images & networks."
 	Write-Host -ForegroundColor Green "	--pai			Prune all images."
@@ -20,61 +27,61 @@ If ($Operation -in "--help") {
 	Write-Host -ForegroundColor Green "	--pac			Prune all containers."
 	Write-Host -ForegroundColor Green "	--p-all			Prune entire system."
 	Write-Host -ForegroundColor Green ""
+	Write-Host -ForegroundColor Green "	[switch]"
+	Write-Host -ForegroundColor Green "	--s 			Sudo switch enable for linux."
 	Write-Host -ForegroundColor Green ""
 	Write-Host -ForegroundColor Green "Usage:"
-	Write-Host -ForegroundColor Green "				docker.ps1 [operation]"
+	Write-Host -ForegroundColor Green "				docker.ps1 [operation] [switch]"
 	Write-Host -ForegroundColor Green ""
 	Exit
-}
-
-If ($Operation -in ("--view", "")) {
+} ElseIf ($Operation -eq "--v") {
 	Write-Host -ForegroundColor Green "=========================================================="
 	Write-Host -ForegroundColor Green "System"
 	Write-Host -ForegroundColor Green "=========================================================="
 	
-	docker system df
+	If ($AddSudo) { sudo } docker system df
 	
 	Write-Host ""
 	Write-Host -ForegroundColor Green "=========================================================="
 	Write-Host -ForegroundColor Green "Networks"
 	Write-Host -ForegroundColor Green "=========================================================="
 	
-	docker network list
+	If ($AddSudo) { sudo } docker network list
 	
 	Write-Host ""
 	Write-Host -ForegroundColor Green "=========================================================="
 	Write-Host -ForegroundColor Green "All Images"
 	Write-Host -ForegroundColor Green "=========================================================="
 	
-	docker images
+	If ($AddSudo) { sudo } docker images
 	
 	Write-Host ""
 	Write-Host -ForegroundColor Green "=========================================================="
 	Write-Host -ForegroundColor Green "All Containers"
 	Write-Host -ForegroundColor Green "=========================================================="
 	
-	docker ps -a
+	If ($AddSudo) { sudo } docker ps -a
 	
 	Write-Host ""
 	Write-Host -ForegroundColor Green "=========================================================="
 	Write-Host -ForegroundColor Green "All Volumes"
 	Write-Host -ForegroundColor Green "=========================================================="
 	
-	docker volume ls
+	If ($AddSudo) { sudo } docker volume ls
 	
 	Write-Host ""
 	Write-Host -ForegroundColor Red "=========================================================="
 	Write-Host -ForegroundColor Red "Dangling Images"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker images -f dangling=true
+	If ($AddSudo) { sudo } docker images -f dangling=true
 	
 	Write-Host ""
 	Write-Host -ForegroundColor Red "=========================================================="
 	Write-Host -ForegroundColor Red "Dangling Volumes"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker volume ls -f dangling=true
+	If ($AddSudo) { sudo } docker volume ls -f dangling=true
 	
 	Write-Host ""
 } ElseIf ($Operation -eq "--pd") {
@@ -82,13 +89,13 @@ If ($Operation -in ("--view", "")) {
 	Write-Host -ForegroundColor Red "Removing Dangling Volumes"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker volume rm -f $(docker volume ls -qf dangling=true)
+	If ($AddSudo) { sudo } docker volume rm -f $(docker volume ls -qf dangling=true)
 	
 	Write-Host -ForegroundColor Red "=========================================================="
 	Write-Host -ForegroundColor Red "Removing Dangling Images"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker rmi -f $(docker images -qf dangling=true)
+	If ($AddSudo) { sudo } docker rmi -f $(docker images -qf dangling=true)
 
 	Write-Host ""
 } ElseIf ($Operation -eq "--pu") {
@@ -96,21 +103,21 @@ If ($Operation -in ("--view", "")) {
 	Write-Host -ForegroundColor Red "Removing Unused Volumes"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker volume prune -f
+	If ($AddSudo) { sudo } docker volume prune -f
 
 	Write-Host ""
 	Write-Host -ForegroundColor Red "=========================================================="
 	Write-Host -ForegroundColor Red "Removing Unused Images"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker image prune -af
+	If ($AddSudo) { sudo } docker image prune -af
 
 	Write-Host ""
 	Write-Host -ForegroundColor Red "=========================================================="
 	Write-Host -ForegroundColor Red "Removing Unused Networks"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker network prune -f
+	If ($AddSudo) { sudo } docker network prune -f
 
 	Write-Host ""
 } ElseIf ($Operation -eq "--pai") {
@@ -118,9 +125,9 @@ If ($Operation -in ("--view", "")) {
 	Write-Host -ForegroundColor Red "Removing All Images"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker rmi -f $(docker images -qf dangling=true)
-	docker rmi -f $(docker images -q)
-	docker image prune -af
+	If ($AddSudo) { sudo } docker rmi -f $(docker images -qf dangling=true)
+	If ($AddSudo) { sudo } docker rmi -f $(docker images -q)
+	If ($AddSudo) { sudo } docker image prune -af
 
 	Write-Host ""
 } ElseIf ($Operation -eq "--pav") {
@@ -128,9 +135,9 @@ If ($Operation -in ("--view", "")) {
 	Write-Host -ForegroundColor Red "Removing All Volumes"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker volume rm (docker volume ls -qf dangling=true)
-	docker volume rm $(docker volume ls)
-	docker volume prune -f
+	If ($AddSudo) { sudo } docker volume rm (docker volume ls -qf dangling=true)
+	If ($AddSudo) { sudo } docker volume rm $(docker volume ls)
+	If ($AddSudo) { sudo } docker volume prune -f
 
 	Write-Host ""
 } ElseIf ($Operation -eq "--pan") {
@@ -138,7 +145,7 @@ If ($Operation -in ("--view", "")) {
 	Write-Host -ForegroundColor Red "Removing All Networks"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker network prune -f
+	If ($AddSudo) { sudo } docker network prune -f
 
 	Write-Host ""
 } ElseIf ($Operation -eq "--pac") {
@@ -146,7 +153,7 @@ If ($Operation -in ("--view", "")) {
 	Write-Host -ForegroundColor Red "Removing All Containers"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker rm -fv $(docker ps -aq)
+	If ($AddSudo) { sudo } docker rm -fv $(docker ps -aq)
 
 	Write-Host ""
 } ElseIf ($Operation -eq "--p-all") {
@@ -154,35 +161,40 @@ If ($Operation -in ("--view", "")) {
 	Write-Host -ForegroundColor Red "Removing All Containers"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker rm -fv $(docker ps -aq)
+	If ($AddSudo) { sudo } docker rm -fv $(docker ps -aq)
 
 	Write-Host -ForegroundColor Red "=========================================================="
 	Write-Host -ForegroundColor Red "Removing All Images"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker rmi -f $(docker images -qf dangling=true)
-	docker rmi -f $(docker images -q)
-	docker image prune -af
+	If ($AddSudo) { sudo } docker rmi -f $(docker images -qf dangling=true)
+	If ($AddSudo) { sudo } docker rmi -f $(docker images -q)
+	If ($AddSudo) { sudo } docker image prune -af
 
 	Write-Host -ForegroundColor Red "=========================================================="
 	Write-Host -ForegroundColor Red "Removing All Volumes"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker volume rm (docker volume ls -qf dangling=true)
-	docker volume rm $(docker volume ls)
-	docker volume prune -f
+	If ($AddSudo) { sudo } docker volume rm (docker volume ls -qf dangling=true)
+	If ($AddSudo) { sudo } docker volume rm $(docker volume ls)
+	If ($AddSudo) { sudo } docker volume prune -f
 
 	Write-Host ""
 	Write-Host -ForegroundColor Red "=========================================================="
 	Write-Host -ForegroundColor Red "Removing All Networks"
 	Write-Host -ForegroundColor Red "=========================================================="
 	
-	docker network prune -f
+	If ($AddSudo) { sudo } docker network prune -f
 
 	Write-Host ""
 } Else {
 	Write-Host -ForegroundColor Red "=========================================================="
-	Write-Host -ForegroundColor Red "Wrong Operation Received"
-	Write-Host -ForegroundColor Red "Use: --view | --pd | --pu | --pai | --pav | --pan | --pac | --p-all"
+	Write-Host -ForegroundColor Red "Expected Operation Parameters"
+	Write-Host -ForegroundColor Red "Use: --h | --v | --pd | --pu | --pai | --pav | --pan | --pac | --p-all"
+	Write-Host -ForegroundColor Red ""
+	Write-Host -ForegroundColor Red "Expected Switch Parameters"
+	Write-Host -ForegroundColor Red "Use: --s"
+	Write-Host -ForegroundColor Red ""
+	Write-Host -ForegroundColor Red "For Help run: docker.ps1 --h"
 	Write-Host -ForegroundColor Red "=========================================================="
 }
